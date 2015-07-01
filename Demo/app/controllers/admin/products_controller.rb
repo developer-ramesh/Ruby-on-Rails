6e4ab0f52659515595d97742003b9d030a9aeeec
ramesh <ramesh@ramesh-PC>
2015-07-02 01:18:18 +0530

    demo project

---
+++ Demo/app/controllers/admin/products_controller.rb
@@ -0,0 +1,34 @@
+class ProductsController < ApplicationController
+  
+  def index
+    render :json => session
+
+  end
+  def show
+   
+  end
+  
+  def new
+  
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+  
+  end
+  
+  def create
+
+  end
+  
+  def destroy
+    
+  end
+  
+  
+  
+  
+end
---
+++ Demo/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '254e20909ce874db0019ea8f32095ee1444a7fc6ec9fbf5757e3dbc80fbcafe69fafaff47d6ceb38a3998720e86db65874ba4b286099afe85889f6402133a194';
